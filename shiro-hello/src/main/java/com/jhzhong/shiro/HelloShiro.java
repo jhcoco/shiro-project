7e877ae26d530aee40c8dd4b4602a97fe64c01d8
jhzhong-win <jhzhong95@gmail.com>
2023-03-10 15:33:25 +0800

    shiro-project 项目目录初始化

---
+++ shiro-hello/src/main/java/com/jhzhong/shiro/HelloShiro.java
@@ -0,0 +1,10 @@
+package com.jhzhong.shiro;
+
+/**
+ * Shiro Hello world!
+ */
+public class HelloShiro {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
